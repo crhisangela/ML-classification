6a272f722696dd8390e47809333b75b21f7d146d
crhisnagela <ferreiracrhis@gmail.com>
2022-03-01 19:46:38 -0300

    iniciando classificacoes web

---
+++ classification/dados.py
@@ -0,0 +1,20 @@
+import csv
+
+import csv
+
+def carregar_acessos():
+    X = []
+    Y = []
+
+    arquivo = open('acesso.csv', 'rb')
+    leitor = csv.reader(arquivo)
+
+    next(leitor)
+
+    for acessou_home,acessou_como_funciona,acessou_contato, comprou in leitor:
+
+        X.append([int(acessou_home),int(acessou_como_funciona)
+            ,int(acessou_contato)])
+        Y.append(int(comprou))
+
+    return X, Y
