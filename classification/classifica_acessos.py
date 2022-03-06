9e65f3439c9494aebf8193baeb96b38cefc3ea58
crhisnagela <ferreiracrhis@gmail.com>
2022-03-06 12:54:16 -0300

    testando algoritmos

--- classification/classifica_acessos.py
+++ classification/classifica_acessos.py
@@ -1,9 +1,26 @@
 from dados import carregar_acessos
 X, Y = carregar_acessos()
 
+treino_ddaos = X[:90]
+treino_marcacoes = Y[:90]
+
+teste_dados = X[-9:]
+teste_marcacoes = Y[-9:]
+
+
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
-modelo.fit(X, Y)
-print(modelo.predict([[1,0,1],[0,1,0],
-                     [1,0,0], [1,1,0], [1,1,1]]))
+modelo.fit(treino_ddaos, treino_marcacoes)
 
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
