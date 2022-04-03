f22dcd477d1698d4b1d6c4eea032467db609cd69
crhisnagela <ferreiracrhis@gmail.com>
2022-04-03 13:04:32 -0300

    end

---
+++ classification/final.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from collections import Counter
+
+df = pd.read_csv('buscas.csv')
+X_df = df[['home','busca', 'logado']]
+Y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values 
+
+porcentagem_de_treino = 0.8
+porcentagem_de_teste = 0.1
+
+tamanho_de_treino = int(porcentagem_de_treino * len(Y))
+tamanho_de_teste = int(porcentagem_de_teste * len(Y))
+tamanho_de_validacao = len(Y) - tamanho_de_treino - tamanho_de_teste
+
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+
+fim_de_teste = tamanho_de_treino + tamanho_de_teste
+
+teste_dados = X[tamanho_de_treino:fim_de_teste]
+teste_marcacoes = Y[tamanho_de_treino:fim_de_teste]
+
+validacao_dados = X[fim_de_teste:]
+validacao_marcacoes = Y[fim_de_teste:]
